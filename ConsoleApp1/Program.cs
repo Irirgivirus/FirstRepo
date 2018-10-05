2c10a36b2da21f88aead3d69b042e0aa2e9a3ce5
Karina <43294718+Irirgivirus@users.noreply.github.com>
2018-10-05 23:31:44 +0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sultanova Karina");
+
+            Console.WriteLine("Введите два числа для подсчета суммы:");
+            //int FirstNumber, SecondNumber;
+            Calc calculation = new Calc();
+            int FirstNumber = Convert.ToInt32(Console.ReadLine());
+            int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int sum = calculation.Add(FirstNumber, SecondNumber);
+
+            Console.WriteLine("Сумма = " + sum);
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+        class Calc
+        {
+            public int Add(int x, int y)
+            {
+                return x + y;
+            }
+        }
+    }
+}
